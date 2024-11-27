167699509f93adbf7b8f68a19c8e1bbb9dfa759e
marianaquesada <99993176+marianaquesada@users.noreply.github.com>
2024-11-27 16:36:10 -0600

    Update repeat

--- TrianglePerez/ide-triangle-v1.1.src/src/Triangle/AbstractSyntaxTrees/RepeatCommand.java
+++ TrianglePerez/ide-triangle-v1.1.src/src/Triangle/AbstractSyntaxTrees/RepeatCommand.java
@@ -6,6 +6,10 @@ public class RepeatCommand extends Command {
 
     public RepeatCommand(Command cAST, Expression eAST, SourcePosition position) {
         super(position);
+        if (cAST == null || eAST == null) {
+            System.err.println("Error: RepeatCommand inicializado con nodos nulos.");
+            throw new IllegalArgumentException("Los nodos de RepeatCommand no pueden ser nulos.");
+        }
         this.C = cAST;
         this.E = eAST;
     }
